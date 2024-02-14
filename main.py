69641f250d71797dd3acd36f1303f90dd8648267
Nihalroshan55 <82870227+Nihalroshan55@users.noreply.github.com>
2024-02-14 05:25:54 +0000

    comments addes

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import numpy as np
 
 arr=np.array([[1,2,3,4,5],[1,2,3,4,5]])
-print(arr[0:2,3:4])
-print(np.shape(arr))
-print(np.size(arr))
-print(np.ndim(arr))
-print(arr.dtype)
-print(type(arr))
+ 
+print(arr[0:2,3:4])     # PRINTING WITH AN INDEX
+
+print(np.shape(arr))    # PRINTING THE SHAPE OF THE ARRAY ( TOTAL NUMBER AND HOW IT PLACED )
+
+print(np.size(arr))     # PRINTING THE SIZE OF THE ARRAY
+
+print(np.ndim(arr))     # PRINTING THE DIMENTIONS OF THE ARRAY
+
+print(arr.dtype)        # PRINTING THE DATA TYPE OF THE ARRAY
+
+print(type(arr))        # PRINDING THE TYPE OF THE ARRAY
