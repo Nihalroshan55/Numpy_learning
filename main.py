b3cfcdc6175a4ff0df48559c5519afcea5f5e704
Nihalroshan55 <82870227+Nihalroshan55@users.noreply.github.com>
2024-02-20 16:01:07 +0000

    added output in the comment

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import numpy as np
 
 arr=np.array([[1,2,3,4,5],[1,2,3,4,5]])
  
-print(arr[0:2,3:4])     # PRINTING WITH AN INDEX
+print(arr[0:2,3:4])     # PRINTING WITH AN INDEX    [[4][4]]
 
-print(np.shape(arr))    # PRINTING THE SHAPE OF THE ARRAY ( TOTAL NUMBER AND HOW IT PLACED )
+print(np.shape(arr))    # PRINTING THE SHAPE OF THE ARRAY ( TOTAL NUMBER AND HOW IT PLACED )    (2, 5)
 
-print(np.size(arr))     # PRINTING THE SIZE OF THE ARRAY
+print(np.size(arr))     # PRINTING THE SIZE OF THE ARRAY    10
 
-print(np.ndim(arr))     # PRINTING THE DIMENTIONS OF THE ARRAY
+print(np.ndim(arr))     # PRINTING THE DIMENTIONS OF THE ARRAY  2
 
-print(arr.dtype)        # PRINTING THE DATA TYPE OF THE ARRAY
+print(arr.dtype)        # PRINTING THE DATA TYPE OF THE ARRAY   int64
 
-print(type(arr))        # PRINDING THE TYPE OF THE ARRAY
+print(type(arr))        # PRINDING THE TYPE OF THE ARRAY    <class 'numpy.ndarray'>
 
-print(np.arange(1,6,2,dtype=float))  # PRINTING AN ARRAY AFTER IT GENERATED ( ARANGE IS WORKING ON arrange( [start] , [stop] , [step] , [dtype]) )
+print(np.arange(1,6,2,dtype=float))  # PRINTING AN ARRAY AFTER IT GENERATED ( ARANGE IS WORKING ON arrange( [start] , [stop] , [step] , [dtype]) )  [1. 3. 5.]
 
 # creating 2 numpy arrays and adding it 
-print(np.arange(1,6),"var1")
-print(np.arange(1,6),"var2")
+print(np.arange(1,6),"var1")    #   [1 2 3 4 5] var1
+print(np.arange(1,6),"var2")    #   [1 2 3 4 5] var2
 var1=np.arange(1,6)
 var2=np.arange(1,6)
 var3=var1+var2  # Here this two array will sum up the element with same index
-print(var3)
+print(var3)     # this is the out put  [ 2  4  6  8 10]
+
+
